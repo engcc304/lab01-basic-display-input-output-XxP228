6762efc6e4c45ed3a845f4354c4eeec0a6997dee
XxP228 <kamochnok.2548@gmail.com>
2023-07-01 15:08:52 +0700

    Lab1

--- Lab/Lab01-Basic-Display-Input-Output.cpp
+++ Lab/Lab01-Basic-Display-Input-Output.cpp
@@ -7,9 +7,9 @@ int main()
     printf("Enter your name:  ");
     scanf("%s", Name);
     printf("Enter your age: ");
-    scanf("%d",&Age);
+    scanf("%d", &Age);
     printf("- - - - - -\n");
-    printf("Hello = %s \n", Name); 
+    printf("Hello = %s\n", Name); 
     printf("Age = %d\n", Age);   
     return 0;
 }
