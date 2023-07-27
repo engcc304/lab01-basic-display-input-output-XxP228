3fdc52bee0cd74d8e90e4f61a6b77e86113c6239
XxP228 <kamochnok.2548@gmail.com>
2023-07-27 14:39:52 +0700

    Lab1-more=more

--- Lab/Lab01-Basic-Display-Input-Output.cpp
+++ Lab/Lab01-Basic-Display-Input-Output.cpp
@@ -3,12 +3,12 @@ int main()
 {
     char Name[50];
     int Age = 0;
-    printf("Enter your name:  ");
-    scanf("%s", Name);
-    printf("Enter your age: ");
+    printf("Enter your name:  ") ;
+    scanf("%s", Name) ;
+    printf("Enter your age: ") ;
     scanf("%d", &Age);
     printf("- - - - - -\n");
-    printf("Hello  %s\n",Name); 
+    printf("Hello %s\n",Name); 
     printf("Age = %d\n",Age);   
     return 0 ;
 }
